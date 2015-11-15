e6a8496c417de57ddb9f5388a8386208b50cc16b
Kopi <ihessmc@gmail.com>
2015-11-15 14:24:01 -0800

    Update LoginMSG.java

--- src/org/iMCCode/LoginMSG/LoginMSG.java
+++ src/org/iMCCode/LoginMSG/LoginMSG.java
@@ -1,1 +1,34 @@
+package org.iMCCode.LoginMSG;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LoginMSG extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		new ListenerClass(this);
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if (cmd.getName().equalsIgnoreCase("loginmsg")){
+			 
+			player.sendMessage(ChatColor.AQUA + "Login" + ChatColor.DARK_RED + "MSG");
+			player.sendMessage(ChatColor.DARK_RED + "Created by" + ChatColor.GRAY + ": " + ChatColor.GREEN + "iHess§7,§a MathBoy§7, and the §5iMCCode Development Team");
+			player.sendMessage(ChatColor.YELLOW + "'When a player logins, the LoginMSG plugin will add a cool touch to their welcome message motd'");
+
+		}
+		return false;	
+		
+	}
+	
+}
